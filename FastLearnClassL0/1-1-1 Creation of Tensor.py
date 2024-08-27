d25a4619d19145fba5acaf84b9c058c039e36963
YoCoco <YoCoco2233@outlook.com>
2024-08-27 11:50:12 +0800

    1-1-1 Creating a tensor of a specified type 2024-8-23 @YoCoco2233

--- FastLearnClassL0/1-1-1 Creation of Tensor.py
+++ FastLearnClassL0/1-1-1 Creation of Tensor.py
@@ -29,10 +29,21 @@ def test02():
     data = torch.Tensor([10])
     print(data)
 
+# 3. 创建指定类型的张量 / Creating a tensor of a specified type
+def test03():
+    # 3.1 创建的时候就指定类型 / Specify the type when creating
+    # 创建一个int32的张量 / # Creating a tensor of int32 type
+    data = torch.IntTensor(2,3)
+    print(data)
 
+    # torch.ShortTensor(2, 3)
+    # torch.LongTensor(2, 3)
+    # torch.floatTensor(2, 3)
 
-
-
+    # 如果创建指定类型的张量，但是传递的数据类型不匹配，会发生类型转换 / If a tensor of a specified type is created but the data type passed does not match, a type conversion will occur.
+    data = torch.IntTensor([2.5,3.5])
+    print(data)
 if __name__ == '__main__':
     test01()
     test02()
+    test03()
